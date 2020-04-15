7b0c7089c3cfb036510dd4c283465bb95d4e29b9
Adnan Raja <58033249+adnannrajaa@users.noreply.github.com>
2020-04-15 10:11:06 +0500

    4/15/2020 Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import Header from './Components/Header.js';
+import React, { Component } from 'react';
 import SearchBar from './Components/SearchBar.js';
 import './App.css';
 
-function App() {
-  return (
-    <div  className="container-fluid">
-      <Header />
-     <SearchBar />
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.data = [];
+    this.state = {
+      search: '',
+      range:3,
+      isLoading: true,
+    };
+  }
+  handleSearchEvents = (title, name) => {
+    this.setState({ [name]: title },()=>{
+      console.log("Search Value  " + this.state.search)
+      console.log("Search Value  " + this.state.range)
+
+    });
+   
+    
+}
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div>
+          <div className="row brand-nav">
+            <img id="wiki-logo" src={`${process.env.PUBLIC_URL}/logo.png`} alt="Wikipedia Logo"></img>
+            <h4 className="brand-name">wikipedia</h4>
+          </div>
+        </div>
+     
+
+        <SearchBar
+          search={this.state.search}
+          range = {this.state.range}
+          handleSearchEvents={this.handleSearchEvents}
+        />
+      </div>
+    )
+  };
 }
 
 export default App;
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    handleOnChange = (e) => {
+        this.props.handleSearchEvents(e.target.value, e.target.name);
+      };
+     
     render() {
         return (
             <div>
 
-              
+
                 <div className="row search-row">
-                    <div className="input-group"  style={{border:`1px solid black`}}>
-                        <input id="search" type="text" className="form-control"></input>
+                    <div className="input-group" style={{ border: `1px solid black` }}>
+                        <input id="search" type="text" className="form-control"
+                          name='search'
+                          value={this.props.search}
+                          onChange={this.handleOnChange}
+                        ></input>
                         <span className="input-group-addon"><i className="fa fa-search"></i> </span>
                     </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <h6 className="text-center">Search on Wikipedia.
+            </h6>
+                    </div>
+                </div>
+                <div className="slidecontainer text-center">
+                    <input type="range" min="1" max="10" className="slider" name="range"
+                     value={this.props.range}
+                     onChange={this.handleOnChange}
+                    ></input>
+                    <p>Value: <span id="demo"></span></p>
                 </div>
             </div>
         );
